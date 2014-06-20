5f6e7cd34c1fc00f42e828d7c089f288f8eb578c
BillytheOrc <turtleswin1337@gmail.com>
2014-06-20 11:48:35 -0600

    Let exploitable code through. Oopsies. Fixed, doe.

--- Mount_At_Lvl_Script.cpp
+++ Mount_At_Lvl_Script.cpp
@@ -1,24 +1,24 @@
 class Mount_At_Lvl_Script : public PlayerScript
 {
 public:
-	Mount_At_Lvl_Script() : PlayerScript("Mount_At_Lvl_Script") { }
-
-	void OnLevelChanged(Player* player, uint8 newLevel)
-	{
-		uint32 SpellID;
-		switch (player->getLevel())
-		{
-		case 20: SpellID = 33388; break;
-		case 40: SpellID = 33391; break;
-		case 67: SpellID = 54197; break; // Cold Weather Flying- Not really important, but whatevz, right?
-		case 70: SpellID = 34091; break;
-		default: return;
-		}
-		player->learnSpell(SpellID, true);
-	}
+        Mount_At_Lvl_Script() : PlayerScript("Mount_At_Lvl_Script") { }
+ 
+ 
+public:
+        void OnLevelChanged(Player* player, uint8 newLevel)
+        {
+                // uint32 SpellID; Unused for this.
+                switch (player->getLevel())
+                {
+                case 20: player->learnSpell(33388, true); break;
+                case 40: player->learnSpell(33391, true); break;
+                case 67: player->learnSpell(54197, true); break; // Cold Weather Flying- Not really important, but whatevz, right?
+                case 70: player->learnSpell(34091, true); break;
+                }
+        }
 };
-
+ 
 void AddSC_Mount_At_Lvl_Script()
 {
-	new Mount_At_Lvl_Script();
+        new Mount_At_Lvl_Script();
 }
